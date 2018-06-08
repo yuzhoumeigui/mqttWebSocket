824d46a06efc1ca5e16371feafbe8af866472fc0
jeffreyWang <yuzhoumeigui@qq.com>
2018-06-08 15:37:17 +0800

    更新

---
+++ public/js/config.js
@@ -0,0 +1,19 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = '47.93.16.183';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 8080;
+topic = 'test';		// topic to subscribe to
+useTLS = false;
+username = 'mqtt_account';
+password = '3c51dbfbd6aeb58e2f1d3c71a05c84ef';
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
